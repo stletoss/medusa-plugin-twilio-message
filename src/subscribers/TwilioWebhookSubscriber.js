4d8e037a43bc7e5411f8592612771d7a72a1156e
stletoss <116314863+stletoss@users.noreply.github.com>
2022-10-21 10:39:50 +0200

    Update TwilioWebhookSubscriber.js

--- src/subscribers/TwilioWebhookSubscriber.js
+++ src/subscribers/TwilioWebhookSubscriber.js
@@ -1,7 +1,12 @@
 class TwilioWebhookSubscriber {
 	constructor({ twilioWebhookService, eventBusService }) {
 		this.twilioWebhookService_ = twilioWebhookService;
+		eventBusService.subscribe("order.placed", this.handleOrder);
 	}
+	
+	handleOrder = async (data) => {
+		return await this.twilioWebhookService_.handleOrder(data.id);
+ 	};
 }
 
 export default TwilioWebhookSubscriber;
