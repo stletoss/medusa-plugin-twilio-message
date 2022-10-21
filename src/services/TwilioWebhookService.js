21f52e7df43c472f1ae981a3c53867bf35b6dbfb
stletoss <116314863+stletoss@users.noreply.github.com>
2022-10-21 10:37:34 +0200

    Update TwilioWebhookService.js

--- src/services/TwilioWebhookService.js
+++ src/services/TwilioWebhookService.js
@@ -1,8 +1,23 @@
 import { BaseService } from "medusa-interfaces";
 
 class TwilioWebhookService extends BaseService {
-	constructor({ }) {
+	constructor({ orderService }) {
 		super();
+		
+		this.orderService_ = orderService;
+	}
+	
+	async handleOrder(orderId) {
+		const { order_discount_total, order_total } = await this.orderService_.retrieve(orderId, {
+			select: [
+				"discount_total",
+				"total",
+			],
+			relations: [
+				"discounts",
+				"discounts.rule",
+			]
+		});
 	}
 }
 
